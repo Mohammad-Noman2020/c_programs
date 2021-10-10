47d2d404a8dff12c66e28fd1c74555aff47fb28c
noman2112 <61879688+noman2112@users.noreply.github.com>
2021-10-10 22:43:00 +0600

    count word

---
+++ countword.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+
+    char sentence[100]="C programming is awesome!";
+    char words[20][60];
+
+
+    int word=0;
+    int flag=0;
+    int index=0;
+
+    for(int i=0; sentence[i]!='\0'; i++)
+    {
+
+
+        if(sentence[i]!=' ' && (sentence[i]>='a'&& sentence[i]<='z')|| (sentence[i]>='A'&& sentence[i]<='Z')|| (sentence[i]>='0'&& sentence[i]<='9'))
+        {
+
+            flag=1;
+            words[index]=
+
+            if(sentence[i+1]=='\0')
+            {
+                word++;
+            }
+            continue;
+
+        }
+        else
+        {
+            if(flag==1)
+            {
+                word++;
+                flag=0;
+                continue;
+
+            }
+            else
+            {
+                flag=0;
+
+                continue;
+            }
+
+
+        }
+
+
+
+
+    }
+    printf("words: %d",word);
+
+
+
+    return 0;
+}
+
